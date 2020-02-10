c5f923b813300a8e057075f813bf1d5a1e70c443
Xavi Moll <xmollv@gmail.com>
2020-02-10 16:06:41 +0100

    Added more factors to be displayed on the right hand side of the graph

--- ResearchKit/Charts/ORKYAxisView.m
+++ ResearchKit/Charts/ORKYAxisView.m
@@ -120,7 +120,8 @@ static const CGFloat ImageVerticalPadding = 3.0;
             if (minimumValue == maximumValue) {
                 _yAxisLabelFactors = @[ @0.5f ];
             } else {
-                _yAxisLabelFactors = @[ @0.2f, @1.0f ];
+//                _yAxisLabelFactors = @[ @0.2f, @1.0f ];
+                _yAxisLabelFactors = @[ @0.0f, @0.1f, @0.2f, @0.3f, @0.4f, @0.5f, @0.6f, @0.7f, @0.8f, @0.9f, @1.0f ];
             }
         }
         
